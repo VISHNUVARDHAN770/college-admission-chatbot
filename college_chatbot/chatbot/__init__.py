ed1ec92099f7e04c7157c35179f2917f4b4fe7d7
VISHNUVARDHAN770 <vishnug1705@gmail.com>
2025-07-04 13:56:24 +0530

    Update __init__.py

--- college_chatbot/chatbot/__init__.py
+++ college_chatbot/chatbot/__init__.py
@@ -1,32 +1,3 @@
-"""from .llm_handler import LLMHandler
-from .knowledge_base import KnowledgeBase
-from .response_handler import ResponseHandler
-
-__version__ = "1.0.0"
-__author__ = "College Admission Team"
-
-__all__ = [
-    "LLMHandler",
-    "KnowledgeBase", 
-    "ResponseHandler"
-]
-
-"""
-
-
-
-
-
-
-"""
-College Admission Chatbot Package
-
-This package contains the core components for the College Admission Chatbot:
-- LLMHandler: Handles language model interactions
-- KnowledgeBase: Manages knowledge base operations and similarity search
-- ResponseHandler: Processes and formats responses
-"""
-
 __version__ = "1.0.0"
 __author__ = "College Admission Chatbot Team"
 __email__ = "admissions@college.edu"
